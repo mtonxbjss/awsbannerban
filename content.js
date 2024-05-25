9f9822a31952ca7097c67d3c46d13d71a2280910
Mark Tonks <marktonks75+github@gmail.com>
2024-05-25 16:32:33 +0100

    Fixed images

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
-// This function hides all div elements with the class 'awsui-context-flashbar', which are the annoying blue AWS flash messages that appear at the top of the page.
+// This const defines all HTML elements that manifest the annoying blue 
+// AWS flash messages that appear at the top of the page and the even
+// more annoying popups that appear when hovering over the bell icon.
 const selectors = [
-  'div[data-analytics-flashbar="info"]',
   'div[data-itemid="opt_out_banner"]',
   'div[data-testid="bellIcon-tooltip-popover"]',
   'div[data-testid="account-services-tooltip-popover"]',
+  'li.awsui_flash-list-item_1q84n_9srp5_301',
+  'li.awsui_flash-list-item_1q84n_n0k4e_301',
+  'div#aperture-csat-container'
 ];
 
 function hideDivsWithClass() {
--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-zip -r  awsbannernuke.xpi manifest.json background.js content.js
+zip -r  awsbannernuke.xpi manifest.json background.js content.js icons/*
 
 echo "All done"
